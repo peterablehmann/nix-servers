bf0e38f514e954963af9ecb3e6eeb5faf336f0cb
Peter Lehmann <36541313+peterablehmann@users.noreply.github.com>
2024-03-15 17:54:45 +0100

    Setup colmena

--- flake.nix
+++ flake.nix
@@ -11,22 +11,57 @@
 
     # SOPS Nix
     sops-nix.url = "github:Mic92/sops-nix";
+
+    # Flake-Utils
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Colmena
+    colmena.url = "github:zhaofengli/colmena/main";
+    colmena.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { self
     , nixpkgs
+    , disko
     , sops-nix
+    , flake-utils
+    , colmena
     , ...
     } @ inputs:
     let
       inherit (self) outputs;
+      conf = self.nixosConfigurations;
+    in
+    (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # pkgs is needed here because colmena would otherwise be in the scope two times
+          pkgs.colmena
+          sops
+          jq
+        ];
+      };
+    })) //
+    {
+      colmena = {
+        # see for details:
+        # https://github.com/zhaofengli/colmena/issues/60#issuecomment-1510496861
+        meta = {
+          nixpkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+          nodeSpecialArgs = builtins.mapAttrs (name: value: value._module.specialArgs) conf;
+        };
+      } // builtins.mapAttrs (name: value: { imports = value._module.args.modules; }) conf;
+
       nixosConfigurations = {
         mns = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           system = "x86_64-linux";
+          extraModules = [ inputs.colmena.nixosModules.deploymentOptions ];
           modules = [
             ./nodes/mns
             self.nixosModules.common
@@ -35,6 +70,7 @@
         monitoring = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           system = "x86_64-linux";
+          extraModules = [ inputs.colmena.nixosModules.deploymentOptions ];
           modules = [
             ./nodes/monitoring
             self.nixosModules.common
@@ -43,6 +79,7 @@
         sync = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           system = "x86_64-linux";
+          extraModules = [ inputs.colmena.nixosModules.deploymentOptions ];
           modules = [
             ./nodes/sync
             self.nixosModules.common
