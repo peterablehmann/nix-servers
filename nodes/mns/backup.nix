6530a2ab1f5b332700e7a5a9e20979fc994d1bc0
Peter Lehmann <xgwq@xnee.net>
2024-05-04 16:48:51 +0200

    Add wasabi S3 as backup target

--- nodes/mns/backup.nix
+++ nodes/mns/backup.nix
@@ -3,7 +3,7 @@
 , ...
 }:
 let
-  mkResticConfig = repository: {
+  mkResticConfig = {repository, environmentFile ? null}: {
     user = "root";
     initialize = true;
     inherit repository;
@@ -11,6 +11,7 @@ let
       OnCalendar = "daily";
       Persistent = true;
     };
+    inherit environmentFile;
     passwordFile = config.sops.secrets."backup/password".path;
     #pruneOpts = [ ];
     paths = [
@@ -32,6 +33,9 @@ in
       "backup/password" = {
         inherit sopsFile;
       };
+      "backup/wasabi" = {
+        inherit sopsFile;
+      };
     };
 
   programs.ssh = {
@@ -44,5 +48,6 @@ in
       "u371467.your-storagebox.de".publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
     };
   };
-  services.restic.backups."${config.networking.hostName}-xgwq" = mkResticConfig "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup";
+  services.restic.backups."${config.networking.hostName}-hetzner" = mkResticConfig {repository = "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup";};
+  services.restic.backups."${config.networking.hostName}-wasabi" = mkResticConfig {repository = "s3:https://s3.eu-central-2.wasabisys.com/backup-xnee-net"; environmentFile = config.sops.secrets."backup/wasabi".path; };
 }
