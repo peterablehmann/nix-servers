00563bfd8037964ab299d2dc207cd5baa0405676
Peter Lehmann <xgwq@xnee.net>
2024-05-19 16:26:53 +0200

    Allow ymir to query node-exporter

--- modules/common/exporters.nix
+++ modules/common/exporters.nix
@@ -3,8 +3,8 @@
     enable = true;
     openFirewall = true;
     firewallRules =
-      "ip saddr 65.108.0.33 tcp dport 9100 accept
-      ip6 saddr 2a01:4f9:6a:4f6f::203 tcp dport 9100 accept
+      "ip saddr 128.140.9.158 tcp dport 9100 accept
+      ip6 saddr 2a01:4f8:c2c:17c9::1 tcp dport 9100 accept
       tcp dport 9100 drop";
     enabledCollectors = [
       "systemd"
