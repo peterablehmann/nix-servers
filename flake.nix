f9bbb7845455908413a74c220f6060c30b152793
Peter Lehmann <xgwq@xnee.net>
2024-06-23 12:17:47 +0200

    Disable Lix for now as it does not evaluate

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Lix
-    lix = {
-      url = "git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
-      flake = false;
-    };
-    lix-module = {
-      url = "git+https://git.lix.systems/lix-project/nixos-module";
-      inputs.lix.follows = "lix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # lix = {
+    #   url = "git+https://git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+    #   flake = false;
+    # };
+    # lix-module = {
+    #   url = "git+https://git.lix.systems/lix-project/nixos-module";
+    #   inputs.lix.follows = "lix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     # Disko
     disko.url = "github:nix-community/disko";
@@ -45,7 +45,7 @@
   outputs =
     { self
     , nixpkgs
-    , lix-module
+    # , lix-module
     , disko
     , sops-nix
     , flake-utils
