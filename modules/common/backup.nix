ad50ef8c895edd0024ab2d9086666dbfec9a1f54
Peter Lehmann <xgwq@xnee.net>
2024-07-17 13:56:10 +0200

    Refactor

--- modules/common/backup.nix
+++ modules/common/backup.nix
@@ -64,8 +64,10 @@ in
           "u371467.your-storagebox.de".publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
         };
       };
-      services.restic.backups."${config.networking.hostName}-hetzner" = mkResticConfig { repository = "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup"; inherit (cfg) paths exclude; };
-      services.restic.backups."${config.networking.hostName}-wasabi" = mkResticConfig { repository = "s3:https://s3.eu-central-2.wasabisys.com/backup-xnee-net"; environmentFile = config.sops.secrets."backup/wasabi".path; inherit (cfg) paths exclude; };
-      services.restic.backups."${config.networking.hostName}-heptifili" = mkResticConfig { repository = "rest:https://restic.heptifili.xnee.net/nix-servers/user"; environmentFile = config.sops.secrets."backup/heptifili".path; inherit (cfg) paths exclude; };
+      services.restic.backups = {
+        "${config.networking.hostName}-hetzner" = mkResticConfig { repository = "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup"; inherit (cfg) paths exclude; };
+        "${config.networking.hostName}-wasabi" = mkResticConfig { repository = "s3:https://s3.eu-central-2.wasabisys.com/backup-xnee-net"; environmentFile = config.sops.secrets."backup/wasabi".path; inherit (cfg) paths exclude; };
+        "${config.networking.hostName}-heptifili" = mkResticConfig { repository = "rest:https://restic.heptifili.xnee.net/nix-servers/user"; environmentFile = config.sops.secrets."backup/heptifili".path; inherit (cfg) paths exclude; };
+      };
     };
 }
