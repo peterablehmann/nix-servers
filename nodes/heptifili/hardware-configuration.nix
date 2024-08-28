c272c2578b07869feb5814641abfa138c3f05e60
Peter Lehmann <xgwq@xnee.net>
2024-08-28 20:42:02 +0200

    Format

--- nodes/heptifili/hardware-configuration.nix
+++ nodes/heptifili/hardware-configuration.nix
@@ -2,15 +2,14 @@
 
 {
   imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    [
+      (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
-  swapDevices = [ ];
+  boot.initrd = {
+    availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+    kernelModules = [ ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
