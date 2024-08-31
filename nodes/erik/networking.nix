61a7de9cbef8ce9743da7bb867e145f5e4c4b60d
Peter Lehmann <xgwq@xnee.net>
2024-08-31 05:51:55 +0200

    Fix Networking

--- nodes/erik/networking.nix
+++ nodes/erik/networking.nix
@@ -4,8 +4,8 @@
 }:
 let
   inherit (config.lib.topology) mkConnectionRev;
-  IPv4 = "192.168.10.11";
-  IPv6 = "fde6:bbc7:8946:7387::100b";
+  IPv4 = "192.168.32.11";
+  IPv6 = "fde6:bbc7:8946:7387::200b";
 in
 {
   topology.self.interfaces.eth0 = {
@@ -30,7 +30,7 @@ in
     usePredictableInterfaceNames = lib.mkDefault false;
     domain = "xnee.net";
     nameservers = [
-      "192.168.10.10"
+      "192.168.32.1"
       "fde6:bbc7:8946:7387:6b4:feff:feca:b60b"
     ];
     timeServers = [ "fde6:bbc7:8946:7387:6b4:feff:feca:b60b" ];
@@ -43,10 +43,10 @@ in
         networkConfig.DHCP = "ipv6";
         matchConfig.Name = "eth0";
         address = [
-          "${IPv4}/23"
+          "${IPv4}/22"
           "${IPv6}/64"
         ];
-        routes = [{ Gateway = "192.168.10.1"; }];
+        routes = [{ Gateway = "192.168.32.1"; }];
         linkConfig.RequiredForOnline = "routable";
       };
     };
