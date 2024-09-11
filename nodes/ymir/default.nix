1264b6c70e4fa8325cf65b3f0720e424154fbfb8
Peter Lehmann <xgwq@xnee.net>
2024-09-11 19:05:51 +0200

    Remove keycloak deployment

--- nodes/ymir/default.nix
+++ nodes/ymir/default.nix
@@ -8,7 +8,6 @@
     ./networking.nix
     inputs.self.nixosModules.monitoring
     inputs.self.nixosModules.kanidm
-    inputs.self.nixosModules.keycloak
     inputs.self.nixosModules.paperless
     inputs.self.nixosModules.radicale
   ];
