0d6806bbae44632ebac8989cc48fea6f0c447e28
Peter Lehmann <xgwq@xnee.net>
2024-12-14 15:47:31 +0100

    Enable Netbootxyz

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -2,7 +2,10 @@
   # Use the systemd-boot EFI boot loader.
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        netbootxyz.enable = true;
+      };
       efi.canTouchEfiVariables = true;
     };
     tmp.cleanOnBoot = true;
