89091f8be6e37b65e52568f6b648910cbe1bf44d
Peter Lehmann <xgwq@xnee.net>
2025-02-07 00:23:27 +0100

    Don't assign the sam IP twice

--- nodes/router-1/networking.nix
+++ nodes/router-1/networking.nix
@@ -5,7 +5,7 @@
 let
   inherit (config.lib.topology) mkConnectionRev;
   IPv4 = "192.168.33.2";
-  IPv6 = "fde6:bbc7:8946:7387::2101";
+  IPv6 = "fde6:bbc7:8946:7387::2102";
 in
 {
   topology.self.interfaces.eth0 = {
