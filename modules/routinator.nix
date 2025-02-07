3822ad1e642ae294735a36cfb0de7bc4a81ff537
Peter Lehmann <xgwq@xnee.net>
2025-02-07 01:22:42 +0100

    Deploy routinator

---
+++ modules/routinator.nix
@@ -0,0 +1,29 @@
+{ config
+, pkgs
+, lib
+, ...
+}:
+let
+  domain = "routinator.xnee.net";
+  tls-dir = config.security.acme.certs.${domain}.directory;
+in
+{
+  networking.domains.subDomains.${domain} = { };
+  security.acme.certs.${domain} = { };
+  services.nginx.virtualHosts."${domain}" = {
+    useACMEHost = domain;
+    kTLS = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "https://[::1]:8323";
+    };
+  };
+
+  systemd.services.kanidm = {
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.routinator} --http-tls=[::1]:8323 --http-tls-key=${tls-dir}/key.pem --http-tls-cert=${tls-dir}/fullchain.pem";
+      SupplementaryGroups = [ config.security.acme.certs.${domain}.group ];
+      BindReadOnlyPaths = [ tls-dir ];
+    };
+  };
+}
