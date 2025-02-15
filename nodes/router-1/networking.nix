1338e06885eff5d2b8050ae8ba1f326c511cae5a
Peter Lehmann <xgwq@xnee.net>
2025-02-15 12:08:20 +0100

    Setup tunnel to AS212895

--- nodes/router-1/networking.nix
+++ nodes/router-1/networking.nix
@@ -37,15 +37,35 @@ in
   };
   systemd.network = {
     enable = true;
-    networks."10-wan" = {
-      networkConfig.DHCP = "ipv6";
-      matchConfig.MACAddress = "BC:24:11:7F:87:15";
-      address = [
-        "${IPv4}/22"
-        "${IPv6}/64"
-      ];
-      routes = [{ Gateway = "192.168.32.1"; }];
-      linkConfig.RequiredForOnline = "routable";
+    netdevs."212895-route64" = {
+      netdevConfig = {
+        Name = "gre-212895";
+        Kind = "gre";
+        MTUBytes = 1480;
+      };
+      tunnelConfig = {
+        Remote = "2a11:6c7:4::1";
+        Local = "fde6:bbc7:8946:7387::200b";
+      };
+    };
+    networks = {
+      "10-wan" = {
+        networkConfig.DHCP = "ipv6";
+        matchConfig.MACAddress = "BC:24:11:7F:87:15";
+        address = [
+          "${IPv4}/22"
+          "${IPv6}/64"
+        ];
+        routes = [{ Gateway = "192.168.32.1"; }];
+        linkConfig.RequiredForOnline = "routable";
+      };
+      "212895-route64" = {
+        matchConfig.Name = config.systemd.network.netdevs."212895-route64".netdevConfig.Name;
+        address = [
+          "100.64.202.174/30"
+          "2a11:6c7:f00:b8::2/64"
+        ];
+      };
     };
   };
 }
