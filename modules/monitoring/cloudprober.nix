e07cd5fefafece918f14efbb80ab4eba31dd31a6
Peter Lehmann <peter.lehmann@hetzner.com>
2025-05-16 07:57:11 +0200

    Expose cloudprober

--- modules/monitoring/cloudprober.nix
+++ modules/monitoring/cloudprober.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
+  domain = "cloudprober.${config.networking.hostName}.xnee.net";
   cloudprober = pkgs.callPackage ./cloudprober-pkgs.nix { };
 in
 {
   imports = [
     ./cloudprober-module.nix
   ];
+
+  security.acme.certs."${domain}" = { };
+
+  networking.domains.subDomains."${domain}" = { };
+
+  services.nginx.virtualHosts."${domain}" = {
+    useACMEHost = domain;
+    kTLS = true;
+    forceSSL = true;
+    locations."/".proxyPass = "http://${config.services.cloudprober.settings.host}:9313";
+  };
 
   services.cloudprober = {
     enable = true;
