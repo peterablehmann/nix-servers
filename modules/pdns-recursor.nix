97be67b5a37ae0babeebd39380696458db269dd0
Peter Lehmann <peter.lehmann@hetzner.com>
2025-06-04 00:24:52 +0200

    Move to yaml config format

--- modules/pdns-recursor.nix
+++ modules/pdns-recursor.nix
@@ -5,6 +5,20 @@
 let
   domain = "dns-rec.${config.networking.hostName}.xnee.net";
   tls-dir = config.security.acme.certs.${domain}.directory;
+  mkForwardZone = zone: {
+    inherit zone;
+    forwarders = [
+      # hydrogen.ns.hetzner.com
+      "213.133.100.98"
+      "2a01:4f8:0:1::add:1098"
+      # oxygen.ns.hetzner.com
+      "88.198.229.192"
+      "2a01:4f8:0:1::add:2992"
+      # helium.ns.hetzner.de
+      "193.47.99.5"
+      "2001:67c:192c::add:5"
+    ];
+  };
 in
 {
   networking.domains.subDomains.${domain} = { };
@@ -18,7 +32,7 @@ in
         return = "404";
       };
       "/metrics" = {
-        proxyPass = "http://[${config.services.pdns-recursor.api.address}]:${builtins.toString config.services.pdns-recursor.api.port}";
+        proxyPass = "http://[${config.services.pdns-recursor.yaml-settings.webservice.address}]:8082";
       };
     };
   };
@@ -29,42 +43,39 @@ in
   };
   services.pdns-recursor = {
     enable = true;
-    serveRFC1918 = true;
-    dnssecValidation = "validate";
-    dns = {
-      allowFrom = [
-        "127.0.0.0/8"
-        "10.0.0.0/8"
-        "100.64.0.0/10"
-        "169.254.0.0/16"
-        "192.168.0.0/16"
-        "172.16.0.0/12"
-        "::1/128"
-        "fc00::/7"
-        "fe80::/10"
-      ];
-      address = [
-        "192.168.33.1"
-        "2003:a:173b:1000::2101"
-      ];
-    };
-    api.address = "::1";
-    settings =
-      let
-        hetzner-nameserver = "213.133.100.98;2a01:4f8:0:1::add:1098;88.198.229.192;2a01:4f8:0:1::add:2992;193.47.99.5;2001:67c:192c::add:5";
-      in
-      {
-        webserver = true;
-        webserver-password = "$scrypt$ln=10,p=1,r=8$LHDOmP4OhG7E86Z6QtsXqQ==$cZlgiI+Y1gFqKh9pvNbJPm74oCs9Wsaqd0JyliSAPQE=";
-        forward-zones = [
-          "bigdriver.net=${hetzner-nameserver}"
-          "hainsacker.de=${hetzner-nameserver}"
-          "lehmann.ing=${hetzner-nameserver}"
-          "lehmann.zone=${hetzner-nameserver}"
-          "uic-fahrzeugnummer.de=${hetzner-nameserver}"
-          "xnee.de=${hetzner-nameserver}"
-          "xnee.net=${hetzner-nameserver}"
+    yaml-settings = {
+      incoming = {
+        listen = [
+          "192.168.33.1"
+          "2003:a:173b:1000::2101"
+        ];
+        allow_from = [
+          "127.0.0.0/8"
+          "192.168.32.0/22"
+          "::1/128"
+          "fe80::/10"
+          "fc00::/7"
+          "2003:a:173b:1000::/56"
         ];
       };
+      dnssec.validation = "validate";
+      recursor = {
+        serve_rfc1918 = true;
+        serve_rfc6303 = true;
+        forward_zones = builtins.map mkForwardZone [
+          "bigdriver.net"
+          "hainsacker.de"
+          "lehmann.ing"
+          "lehmann.zone"
+          "uic-fahrzeugnummer.de"
+          "xnee.de"
+          "xnee.net"
+        ];
+      };
+      webservice = {
+        address = "::1";
+        password = "$scrypt$ln=10,p=1,r=8$LHDOmP4OhG7E86Z6QtsXqQ==$cZlgiI+Y1gFqKh9pvNbJPm74oCs9Wsaqd0JyliSAPQE=";
+      };
+    };
   };
 }
