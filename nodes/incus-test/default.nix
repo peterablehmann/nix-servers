5b79b6ab364d08c6940024793ec45d859b728b7a
Peter Lehmann <peter.lehmann@hetzner.com>
2025-06-27 16:01:31 +0200

    Proxy WebUI

--- nodes/incus-test/default.nix
+++ nodes/incus-test/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   ...
 }:
@@ -12,6 +13,16 @@
   metadata = {
     ipv4 = false;
     ipv6 = true;
+  };
+
+  security.acme.certs."${config.networking.fqdn}" = { };
+  services.nginx.virtualHosts."${config.networking.fqdn}" = {
+    useACMEHost = config.networking.fqdn;
+    kTLS = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "https://[::1]:8443";
+    };
   };
 
   virtualisation.incus = {
