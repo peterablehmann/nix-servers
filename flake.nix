24faf43eb7c63f4dedd738b15d242a2b88832570
Peter Lehmann <peter.lehmann@hetzner.com>
2025-06-29 08:23:25 +0200

    Remove incus-test

--- flake.nix
+++ flake.nix
@@ -120,15 +120,6 @@
             self.nixosModules.common
           ];
         };
-        incus-test = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          system = "x86_64-linux";
-          extraModules = [ inputs.colmena.nixosModules.deploymentOptions ];
-          modules = [
-            ./nodes/incus-test
-            self.nixosModules.common
-          ];
-        };
       };
 
       nixosModules = {
