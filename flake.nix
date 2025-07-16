95619221a63bc2bbad5eb56d633dcb3295349f8f
Peter Lehmann <peter.lehmann@hetzner.com>
2025-07-16 18:16:15 +0200

    Don't fix lix

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Lix
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # lix-module = {
+    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     # Disko
     disko.url = "github:nix-community/disko";
@@ -44,7 +44,7 @@
     {
       self,
       nixpkgs,
-      lix-module,
+      # lix-module,
       disko,
       sops-nix,
       flake-utils,
