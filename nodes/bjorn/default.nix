15851ab6b3f60e4aacd36223aa18e7b49cb0bc5b
Peter Lehmann <peter.lehmann@hetzner.com>
2025-07-16 17:44:44 +0200

    Deploy some runners for nix-as213422

--- nodes/bjorn/default.nix
+++ nodes/bjorn/default.nix
@@ -18,18 +18,51 @@
 
   services.qemuGuest.enable = true;
 
-  sops.secrets."github-runners/nix-as213422".sopsFile =
-    "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+  sops.secrets = {
+    "github-runners/nix-as213422-1".sopsFile =
+      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+    "github-runners/nix-as213422-2".sopsFile =
+      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+    "github-runners/nix-as213422-3".sopsFile =
+      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+    "github-runners/nix-as213422-4".sopsFile =
+      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+  };
 
-  services.github-runners = {
-    nix-as213422 = {
-      enable = true;
-      name = "nix-as213422";
-      tokenFile = config.sops.secrets."github-runners/nix-as213422".path;
-      url = "https://github.com/peterablehmann/nix-as213422";
+  services.github-runners =
+    let
       extraPackages = with pkgs; [
         openssh
       ];
+    in
+    {
+      nix-as213422-1 = {
+        enable = true;
+        name = "nix-as213422-1";
+        tokenFile = config.sops.secrets."github-runners/nix-as213422-1".path;
+        url = "https://github.com/peterablehmann/nix-as213422";
+        inherit extraPackages;
+      };
+      nix-as213422-2 = {
+        enable = true;
+        name = "nix-as213422-2";
+        tokenFile = config.sops.secrets."github-runners/nix-as213422-2".path;
+        url = "https://github.com/peterablehmann/nix-as213422";
+        inherit extraPackages;
+      };
+      nix-as213422-3 = {
+        enable = true;
+        name = "nix-as213422-3";
+        tokenFile = config.sops.secrets."github-runners/nix-as213422-3".path;
+        url = "https://github.com/peterablehmann/nix-as213422";
+        inherit extraPackages;
+      };
+      nix-as213422-4 = {
+        enable = true;
+        name = "nix-as213422-4";
+        tokenFile = config.sops.secrets."github-runners/nix-as213422-4".path;
+        url = "https://github.com/peterablehmann/nix-as213422";
+        inherit extraPackages;
+      };
     };
-  };
 }
