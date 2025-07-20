ef0f8d8952092a29bed4f0d125c7416a1675342c
Peter Lehmann <peter.lehmann@hetzner.com>
2025-07-20 23:51:11 +0200

    Remove lix cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -15,12 +15,6 @@
         "nix-command"
         "flakes"
       ];
-      substituters = [
-        "https://cache.lix.systems"
-      ];
-      trusted-public-keys = [
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      ];
     };
     nixPath = [ "nixpkgs=flake:nixpkgs" ];
     gc = {
