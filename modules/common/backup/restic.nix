0b74821419e141b66349277d46d9e4deab7aa604
Peter Lehmann <peter.lehmann@hetzner.com>
2025-08-02 16:11:59 +0200

    Autodiscover restic server backup targets

--- modules/common/backup/restic.nix
+++ modules/common/backup/restic.nix
@@ -55,7 +55,7 @@ in
         "backup/wasabi" = {
           inherit sopsFile;
         };
-        "backup/heptifili" = {
+        "backup/restic-server" = {
           inherit sopsFile;
         };
         "backup/hetzner-s3" = {
@@ -74,24 +74,39 @@ in
           "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
       };
     };
-    services.restic.backups = {
-      "${config.networking.hostName}-hetzner" = mkResticConfig {
-        repository = "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup";
-        inherit (cfg) paths exclude;
-      };
-      "${config.networking.hostName}-wasabi" = mkResticConfig {
-        repository = "s3:https://s3.eu-central-2.wasabisys.com/backup-xnee-net";
-        environmentFile = config.sops.secrets."backup/wasabi".path;
-        inherit (cfg) paths exclude;
-      };
-      "${config.networking.hostName}-hetzner-s3" = mkResticConfig {
-        repository = "s3:https://nbg1.your-objectstorage.com/backup-xnee-net";
-        environmentFile = config.sops.secrets."backup/hetzner-s3".path;
-        inherit (cfg) paths exclude;
-      };
-    };
-    # // lib.attrsets.optionalAttrs (config.networking.fqdn != "heptifili.xnee.net") {
-    #   "${config.networking.hostName}-heptifili" = mkResticConfig { repository = "rest:https://restic.heptifili.xnee.net/nix-servers"; environmentFile = config.sops.secrets."backup/heptifili".path; inherit (cfg) paths exclude; };
-    # };
+    services.restic.backups =
+      {
+        "restic-backup-hetzner" = mkResticConfig {
+          repository = "sftp://u371467-sub2@u371467.your-storagebox.de:22//backup";
+          inherit (cfg) paths exclude;
+        };
+        "restic-backup-wasabi" = mkResticConfig {
+          repository = "s3:https://s3.eu-central-2.wasabisys.com/backup-xnee-net";
+          environmentFile = config.sops.secrets."backup/wasabi".path;
+          inherit (cfg) paths exclude;
+        };
+        "restic-backup-hetzner-s3" = mkResticConfig {
+          repository = "s3:https://nbg1.your-objectstorage.com/backup-xnee-net";
+          environmentFile = config.sops.secrets."backup/hetzner-s3".path;
+          inherit (cfg) paths exclude;
+        };
+      }
+      // (builtins.listToAttrs (
+        lib.mapAttrsToList
+          (name: host: {
+            name = "restic-backup-${host.config.networking.hostName}";
+            value = mkResticConfig {
+              repository = "rest:https://restic.${host.config.networking.fqdn}/nix-servers";
+              environmentFile = config.sops.secrets."backup/restic-server".path;
+              inherit (cfg) paths exclude;
+            };
+          })
+          (
+            lib.filterAttrs (
+              name: host:
+              (host.config.networking.fqdn != config.networking.fqdn && host.config.services.restic.server.enable)
+            ) inputs.self.nixosConfigurations
+          )
+      ));
   };
 }
