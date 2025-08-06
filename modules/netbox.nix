f2f8fb1b39e14d357dc78012d2e068b0db47ea46
Peter Lehmann <peter.lehmann@hetzner.com>
2025-08-06 12:22:29 +0200

    Setup SSO for netbox

--- modules/netbox.nix
+++ modules/netbox.nix
@@ -8,10 +8,17 @@ let
   domain = "netbox.xnee.net";
 in
 {
-  sops.secrets."netbox/secret_key" = {
-    sopsFile = "${inputs.self}/secrets/${config.networking.hostName}.yaml";
-    owner = "netbox";
-    group = "netbox";
+  sops.secrets = {
+    "netbox/secret_key" = {
+      sopsFile = "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+      owner = "netbox";
+      group = "netbox";
+    };
+    "netbox/environment" = {
+      sopsFile = "${inputs.self}/secrets/${config.networking.hostName}.yaml";
+      owner = "netbox";
+      group = "netbox";
+    };
   };
   networking.domains.subDomains.${domain} = { };
   security.acme.certs."${domain}" = { };
@@ -37,12 +44,26 @@ in
     enable = true;
     package = pkgs.netbox;
     secretKeyFile = config.sops.secrets."netbox/secret_key".path;
-    settings.ALLOWED_HOSTS = [ domain ];
+    settings = {
+      ALLOWED_HOSTS = [ domain ];
+      REMOTE_AUTH_ENABLED = "True";
+      REMOTE_AUTH_BACKEND = "social_core.backends.open_id_connect.OpenIdConnectAuth";
+      SOCIAL_AUTH_OIDC_OIDC_ENDPOINT = "https://idm.xnee.net/oauth2/openid/netbox";
+      SOCIAL_AUTH_OIDC_KEY = "netbox";
+      SOCIAL_AUTH_OIDC_JWT_ALGORITHMS = [ "ES256" ];
+    };
+    extraConfig = ''
+      from os import environ
+      SOCIAL_AUTH_OIDC_SECRET = environ.get('SOCIAL_AUTH_OIDC_SECRET')
+    '';
   };
 
-  systemd.services.nginx.serviceConfig = {
-    SupplementaryGroups = [ config.systemd.services.netbox.serviceConfig.Group ];
-    BindReadOnlyPaths = [ config.services.netbox.dataDir ];
+  systemd.services = {
+    netbox.serviceConfig.EnvironmentFile = config.sops.secrets."netbox/environment".path;
+    nginx.serviceConfig = {
+      SupplementaryGroups = [ config.systemd.services.netbox.serviceConfig.Group ];
+      BindReadOnlyPaths = [ config.services.netbox.dataDir ];
+    };
   };
 
   backup.paths = [ config.services.netbox.dataDir ];
