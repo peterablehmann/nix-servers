0a0b30f5c01d52965f8b2a3e0aad560a13b82e1f
Peter Lehmann <xgwq@xnee.net>
2025-10-30 20:41:39 +0100

    Alert via mail on RAs

--- nodes/ixpect-frankonix/default.nix
+++ nodes/ixpect-frankonix/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   ...
 }:
@@ -21,6 +22,8 @@
       };
     };
   };
+
+  sops.secrets."mail/pass".sopsFile = "${inputs.self}/secrets/${config.networking.hostName}.yaml";
 
   networking.firewall.trustedInterfaces = [ "enx90a182722eb1" ];
 
@@ -78,29 +81,51 @@
           ipv6_router.enable = true;
           stp.enable = true;
         };
-        event.notifiers.log = {
-          enable = true;
-          events = [
-            "ARP_BOGON_SOURCE"
-            "ARP_BOGON_TARGET"
-            "ARP_NEIGHBOR_NEW_DYNAMIC"
-            "ARP_NEIGHBOR_SPOOFED_DYNAMIC"
-            "ARP_NEIGHBOR_SPOOFED_STATIC"
-            "ARP_NEIGHBOR_UNKNOWN"
-            "BUM_RATE_BROADCAST_EXCEEDED"
-            "BUM_RATE_MULTICAST_EXCEEDED"
-            "BUM_RATE_UNICAST_EXCEEDED"
-            "ETHER_TYPE_VIOLATION"
-            "IPV6_BOGON_SOURCE"
-            "IPV6_BOGON_TARGET"
-            "IPV6_NEIGHBOR_NEW_DYNAMIC"
-            "IPV6_NEIGHBOR_SPOOFED_DYNAMIC"
-            "IPV6_NEIGHBOR_SPOOFED_STATIC"
-            "IPV6_NEIGHBOR_UNKNOWN"
-            "IPV6_ROUTER_ADVERTISEMENT"
-            "IPV6_ROUTER_SOLICITATION"
-            "STP_PACKET_FOUND"
-          ];
+        event.notifiers = {
+          log = {
+            enable = true;
+            events = [
+              "ARP_BOGON_SOURCE"
+              "ARP_BOGON_TARGET"
+              "ARP_NEIGHBOR_NEW_DYNAMIC"
+              "ARP_NEIGHBOR_SPOOFED_DYNAMIC"
+              "ARP_NEIGHBOR_SPOOFED_STATIC"
+              "ARP_NEIGHBOR_UNKNOWN"
+              "BUM_RATE_BROADCAST_EXCEEDED"
+              "BUM_RATE_MULTICAST_EXCEEDED"
+              "BUM_RATE_UNICAST_EXCEEDED"
+              "ETHER_TYPE_VIOLATION"
+              "IPV6_BOGON_SOURCE"
+              "IPV6_BOGON_TARGET"
+              "IPV6_NEIGHBOR_NEW_DYNAMIC"
+              "IPV6_NEIGHBOR_SPOOFED_DYNAMIC"
+              "IPV6_NEIGHBOR_SPOOFED_STATIC"
+              "IPV6_NEIGHBOR_UNKNOWN"
+              "IPV6_ROUTER_ADVERTISEMENT"
+              "IPV6_ROUTER_SOLICITATION"
+              "STP_PACKET_FOUND"
+            ];
+          };
+          email = {
+            enable = true;
+            smtp = {
+              host = "mail.your-server.de";
+              port = 587;
+              encryption = "TLS";
+              auth = {
+                mechanism = "PLAIN";
+                user = "noreply@xnee.net";
+                pass.file = config.sops.secrets."mail/pass".path;
+              };
+            };
+            from = "noreply+ixpect@xnee.net";
+            channels = [
+              {
+                to = "alert@as213422.net";
+                events = [ "IPV6_ROUTER_ADVERTISEMENT" ];
+              }
+            ];
+          };
         };
       };
     };
