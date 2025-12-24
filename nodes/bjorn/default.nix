248942f801568ec2b86ca4421e8acdb4f0cbfa15
Peter Lehmann <xgwq@xnee.net>
2025-12-24 14:42:00 +0100

    Update runner tokens

--- nodes/bjorn/default.nix
+++ nodes/bjorn/default.nix
@@ -31,14 +31,6 @@
   services.qemuGuest.enable = true;
 
   sops.secrets = {
-    "github-runners/nix-as213422-1".sopsFile =
-      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
-    "github-runners/nix-as213422-2".sopsFile =
-      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
-    "github-runners/nix-as213422-3".sopsFile =
-      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
-    "github-runners/nix-as213422-4".sopsFile =
-      "${inputs.self}/secrets/${config.networking.hostName}.yaml";
     "github-runners/offline-kollektiv-xnee-net-1".sopsFile =
       "${inputs.self}/secrets/${config.networking.hostName}.yaml";
     "github-runners/offline-kollektiv-xnee-net-2".sopsFile =
@@ -52,34 +44,6 @@
       ];
     in
     {
-      nix-as213422-1 = {
-        enable = true;
-        name = "nix-as213422-1";
-        tokenFile = config.sops.secrets."github-runners/nix-as213422-1".path;
-        url = "https://github.com/peterablehmann/nix-as213422";
-        inherit extraPackages;
-      };
-      nix-as213422-2 = {
-        enable = true;
-        name = "nix-as213422-2";
-        tokenFile = config.sops.secrets."github-runners/nix-as213422-2".path;
-        url = "https://github.com/peterablehmann/nix-as213422";
-        inherit extraPackages;
-      };
-      nix-as213422-3 = {
-        enable = true;
-        name = "nix-as213422-3";
-        tokenFile = config.sops.secrets."github-runners/nix-as213422-3".path;
-        url = "https://github.com/peterablehmann/nix-as213422";
-        inherit extraPackages;
-      };
-      nix-as213422-4 = {
-        enable = true;
-        name = "nix-as213422-4";
-        tokenFile = config.sops.secrets."github-runners/nix-as213422-4".path;
-        url = "https://github.com/peterablehmann/nix-as213422";
-        inherit extraPackages;
-      };
       offline-kollektiv-xnee-net-1 = {
         enable = true;
         name = "offline-kollektiv-xnee-net-1";
