8c1f6b8dc050f44ef3ec8586200d974ea83dab80
Peter Lehmann <xgwq@xnee.net>
2026-08-29 20:39:04 +0200

    Fix socket

--- modules/netbox.nix
+++ modules/netbox.nix
@@ -36,7 +36,7 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://unix:${config.services.netbox.bind}";
+          proxyPass = "http://${config.services.netbox.bind}";
         };
         "/static/" = {
           alias = "${config.services.netbox.dataDir}/static/";
