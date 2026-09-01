9454fca8649529fcbf52a3377271e33ded304acf
Peter Lehmann <xgwq@xnee.net>
2026-09-01 18:08:04 +0200

    stigr: setup haproxy

--- nodes/stigr/default.nix
+++ nodes/stigr/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   ...
 }:
@@ -27,4 +28,31 @@
   };
 
   services.qemuGuest.enable = true;
+
+  services.nginx.defaultListenAddresses = [ "[::1]" ];
+
+  services.haproxy = {
+    enable = true;
+    config = ''
+      frontend http-in
+        mode tcp
+        tcp-request inspect-delay 5s
+        bind ${config.metadata.network.ipv4.address}:80
+        bind [${config.metadata.network.ipv6.address}]:80
+        default_backend http-local
+
+      frontend https-in
+        mode tcp
+        tcp-request inspect-delay 5s
+        bind ${config.metadata.network.ipv4.address}:443
+        bind [${config.metadata.network.ipv6.address}]:443
+        default_backend https-local
+      
+      backend http-local
+        server local [::1]:80
+
+      backend https-local
+        server local [::1]:443
+    '';
+  };
 }
