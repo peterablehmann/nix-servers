e721cffe5ce130975df7f0978dcf88ba04ac4184
Peter Lehmann <xgwq@xnee.net>
2026-09-03 18:42:30 +0200

    format

--- nodes/stigr/default.nix
+++ nodes/stigr/default.nix
@@ -27,32 +27,32 @@
     };
   };
 
-  services.qemuGuest.enable = true;
+  services = {
+    qemuGuest.enable = true;
+    nginx.defaultListenAddresses = [ "[::1]" ];
+    haproxy = {
+      enable = true;
+      config = ''
+        frontend http-in
+          mode tcp
+          tcp-request inspect-delay 5s
+          bind ${config.metadata.network.ipv4.address}:80
+          bind [${config.metadata.network.ipv6.address}]:80
+          default_backend http-local
 
-  services.nginx.defaultListenAddresses = [ "[::1]" ];
+        frontend https-in
+          mode tcp
+          tcp-request inspect-delay 5s
+          bind ${config.metadata.network.ipv4.address}:443
+          bind [${config.metadata.network.ipv6.address}]:443
+          default_backend https-local
 
-  services.haproxy = {
-    enable = true;
-    config = ''
-      frontend http-in
-        mode tcp
-        tcp-request inspect-delay 5s
-        bind ${config.metadata.network.ipv4.address}:80
-        bind [${config.metadata.network.ipv6.address}]:80
-        default_backend http-local
+        backend http-local
+          server local [::1]:80
 
-      frontend https-in
-        mode tcp
-        tcp-request inspect-delay 5s
-        bind ${config.metadata.network.ipv4.address}:443
-        bind [${config.metadata.network.ipv6.address}]:443
-        default_backend https-local
-      
-      backend http-local
-        server local [::1]:80
-
-      backend https-local
-        server local [::1]:443
-    '';
+        backend https-local
+          server local [::1]:443
+      '';
+    };
   };
 }
