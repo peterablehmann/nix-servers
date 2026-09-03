5b5085ccb7786200847b3a862e53e9b4cac3e19b
Peter Lehmann <xgwq@xnee.net>
2026-09-03 18:56:22 +0200

    stigr: Drop HAProxy test

--- nodes/stigr/default.nix
+++ nodes/stigr/default.nix
@@ -27,32 +27,5 @@
     };
   };
 
-  services = {
-    qemuGuest.enable = true;
-    nginx.defaultListenAddresses = [ "[::1]" ];
-    haproxy = {
-      enable = true;
-      config = ''
-        frontend http-in
-          mode tcp
-          tcp-request inspect-delay 5s
-          bind ${config.metadata.network.ipv4.address}:80
-          bind [${config.metadata.network.ipv6.address}]:80
-          default_backend http-local
-
-        frontend https-in
-          mode tcp
-          tcp-request inspect-delay 5s
-          bind ${config.metadata.network.ipv4.address}:443
-          bind [${config.metadata.network.ipv6.address}]:443
-          default_backend https-local
-
-        backend http-local
-          server local [::1]:80
-
-        backend https-local
-          server local [::1]:443
-      '';
-    };
-  };
+  services.qemuGuest.enable = true;
 }
