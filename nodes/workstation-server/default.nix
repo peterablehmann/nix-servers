cadb763e76daf27214df381db2dea89cf1982b48
Peter Lehmann <xgwq@xnee.net>
2026-09-04 23:16:34 +0200

    workstation-server: change IP

--- nodes/workstation-server/default.nix
+++ nodes/workstation-server/default.nix
@@ -17,12 +17,12 @@
     network = {
       ipv4 = {
         dns = false;
-        address = "192.168.48.5";
+        address = "192.168.48.7";
         prefixLength = 24;
         gateway = "192.168.48.1";
       };
       ipv6 = {
-        address = "2a01:4f8:1b7:731::5";
+        address = "2a01:4f8:1b7:731::7";
         prefixLength = 64;
         gateway = "2a01:4f8:1b7:731::1";
       };
